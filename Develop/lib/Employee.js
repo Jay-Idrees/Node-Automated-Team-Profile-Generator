9880aa69aa3abb713a772559c028089b0f6bd06a
Jay Idrees <idreesjay@gmail.com>
2020-11-14 13:36:33 -0500

    Created employee class using the constructor function

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,35 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+
+    constructor (name, id, email) {
+ //This constructor function is basically creating an 'employee' object and turing the values received as parameters into variables (also called member variables or properties)
+ // This value that you have received in as parameter as name -turn it into a variable called name
+        this.name=name
+        this.id=id
+        this.email=email
+
+    }// closng  br for the costructor function
+
+// More fuctions inside the employee class
+
+//obtaining the name, id and email as needed
+
+getName(){
+    return this.name
+}
+
+
+getId(){
+    return this.id
+}
+
+getEmail(){
+    return this.id
+}
+
+getRole(){
+    return "Employee"
+}
+
+
+} //closing br for the employee class
