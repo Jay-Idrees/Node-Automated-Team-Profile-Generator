f7ce35d055a1b109bf3af6c4e67ca2e72da6fa22
Jay Idrees <idreesjay@gmail.com>
2020-11-14 15:58:12 -0500

    Added the manager class as extension of the employee class

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,23 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee=require('./Employee')
+
+//Creating the manage class as extension of the Employee class
+class Manager extends Employee {
+
+    constructor (name, id, email, officeNumber){
+// Here I am borrowing the name, id and email properties from the Employee function
+        super(name, id, email)
+        this.officeNumber=officeNumber
+
+    }// cl br for the constructor funtion
+
+getOfficeNumber (){
+    return this.officeNumber
+}
+
+getRole(){
+    return 'Manager'
+}
+
+}// cl br for employee function
+
+module.exports=Manager
