1877e594773d5d6234e6e7b6c4a18059a6935483
Jay Idrees <idreesjay@gmail.com>
2020-11-14 15:46:35 -0500

    updated intern class

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,17 +1,20 @@
 const Employee=require("./Employee");
 
+// Creating the intern class as an extension of the Employee class
 class Intern extends Employee {
 
+    // Notice that I have name , id, email and shcool(new), of these the name, id and email properties will be pulled from the employee class using super
     constructor (name, id, email, school){
-    super (name, id, email, school)
-    this.school=school
+    super (name, id, email);
+
+    this.school=school;
     }
     getSchool(){
         return this.school;
     };
 
     getRole(){
-        return "Intern"
+        return "Intern";
     }
 }
 module.exports-Intern
